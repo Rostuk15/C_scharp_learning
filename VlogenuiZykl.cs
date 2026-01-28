491b17c14569e1c1d2b802f698a03850e303a983
Rostyk <kremen.rostylsav@gmail.com>
2026-01-28 15:46:47 +0100

    T_narnie-exerciesses

--- VlogenuiZykl.cs
+++ VlogenuiZykl.cs
@@ -9,7 +9,7 @@ class Program
     {
     }
 
-    static void Main(string[] args)
+    static void VlogeniyMain(string[] args)
     {
         VlogeniyZyklWork();
         HomeWork();
--- obj/Debug/net10.0/C#learning.AssemblyInfo.cs
+++ obj/Debug/net10.0/C#learning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf571893eb3849335c854388e48b0822d8d2544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671670bf50bdbf4555b8b808cc3487d7914cb640")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
